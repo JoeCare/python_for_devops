91ae486ec2a9d6ea625755552f35421eaceaabdc
Joseph Careless <gjwer@spacer>
2024-09-21 23:53:57 +0200

    Operations on sequences with builtins

--- exc1.py
+++ exc1.py
@@ -6,9 +6,17 @@ def fib_rec(i: int) -> int:
  
 squares = list(map(lambda x: x**2, range(10)))
 fib_sequence = list(map(fib_rec, range(20)))
+
+print(squares, fib_sequence)
+print(squares, fib_sequence)
 print(squares, fib_sequence)
 
 my_list = [("apple", 5), ("lemon", 2), ("orange", 1)]
 my_dict = dict(my_list)
 print(my_dict)
-print(sorted(list(map(lambda x: x[1], my_dict.items()))))
+print()
+
+my_list_sorted = sorted(list(map(lambda x: x[1], my_dict.items())))
+my_dict_sorted_by_key = dict(sorted(my_dict.items(), key=lambda x: x[1]))
+print(list(zip(my_dict.values(), my_dict.keys())))
+
