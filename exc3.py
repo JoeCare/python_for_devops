f6a36106308f7da3032404817f2b6775d9dd7668
Joseph Careless <gjwer@spacer>
2024-09-22 14:33:09 +0200

    exc3.py

--- exc3.py
+++ exc3.py
@@ -3,6 +3,8 @@ import re
 cc_list = """
 Ezra Koenig <ekoenig@vpwk.com>,
 Rostam Badming <rbadmin@vpwk.com>,
+Rostar Wadwin
+Bostam Radlin
 Chris Tomson <ctomson@vpwk.com>,
 Bobbi Baio <bbaio@vpwk.com>
 """
@@ -12,4 +14,14 @@ else:
     print('None')
 
 if re.search(r'Rostam', cc_list):
-    print(re.search(r'Rostam', cc_list))
+    print("Match for", re.search(r'Rostam', cc_list))
+
+search_for_1 = r'[B, x]osta[r, m]'
+search_in = re.search(search_for_1, cc_list)
+findall_in = re.findall(search_for_1, cc_list)
+
+if search_in:
+    print("Match for:", search_in)
+
+if findall_in:
+    print("Found all:", findall_in)
